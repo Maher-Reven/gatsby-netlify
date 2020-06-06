71b171af4a62131e2cf0cee6653b5d33a63445fd
Maher Kurdi <m.saadkrde@ltp.com>
2020-06-06 18:02:44 +0200

    add okta for auth

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+
 export default () => {
-  return (
-      <>
-        Hello world!
-        <p><Link to="/blog">View Blog</Link></p>
-      </>)
-} 
+    return (
+        <>
+            Hello world!
+            <p><Link to="/blog">View Blog</Link></p>
+            <p><Link to="/account">My Account</Link></p>
+        </>)
+}
